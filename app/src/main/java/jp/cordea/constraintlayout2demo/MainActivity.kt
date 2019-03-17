bc744f560366cf19eb2e37e23fb365230809aeed
Yoshihiro Tanaka <contact@cordea.jp>
2019-03-17 12:08:07 +0900

    add fab animation

--- app/src/main/java/jp/cordea/constraintlayout2demo/MainActivity.kt
+++ app/src/main/java/jp/cordea/constraintlayout2demo/MainActivity.kt
@@ -1,11 +1,11 @@
 package jp.cordea.constraintlayout2demo
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,11 +14,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
