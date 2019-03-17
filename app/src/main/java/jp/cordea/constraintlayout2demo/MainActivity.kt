84fc5ad9224a7077bcdbdba2e54cc9affc23fe51
Yoshihiro Tanaka <contact@cordea.jp>
2019-03-17 16:13:04 +0900

    fix order of fab

--- app/src/main/java/jp/cordea/constraintlayout2demo/MainActivity.kt
+++ app/src/main/java/jp/cordea/constraintlayout2demo/MainActivity.kt
@@ -1,24 +1,22 @@
 package jp.cordea.constraintlayout2demo
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import jp.cordea.constraintlayout2demo.databinding.ActivityMainBinding
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        setSupportActionBar(binding.toolbar)
 
-        binding.fab.setOnClickListener {
-            binding.fab1.isClickable = true
-        }
         binding.fab1.setOnClickListener {
+            startActivity(Intent(this, SubActivity::class.java))
         }
     }
 
