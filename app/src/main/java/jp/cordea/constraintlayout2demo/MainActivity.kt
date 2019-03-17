207e956aede13a2d3cf13f31af309183c301290c
Yoshihiro Tanaka <contact@cordea.jp>
2019-03-17 14:33:30 +0900

    enable databinding

--- app/src/main/java/jp/cordea/constraintlayout2demo/MainActivity.kt
+++ app/src/main/java/jp/cordea/constraintlayout2demo/MainActivity.kt
@@ -4,28 +4,30 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.motion.widget.MotionLayout
-import com.google.android.material.snackbar.Snackbar
+import androidx.databinding.DataBindingUtil
+import jp.cordea.constraintlayout2demo.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        binding.fab.setOnClickListener {
+            binding.fab1.isClickable = true
+        }
+        binding.fab1.setOnClickListener {
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
